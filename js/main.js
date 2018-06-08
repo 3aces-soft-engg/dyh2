7ac33dfb8fb9b7e4611845da9549c068330d8e42
3aces-soft-engg <gaurav.sharma@3acestech.com>
2018-06-08 18:09:23 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var menu_open = document.getElementsByClassName('menu-func');
 var popBlock = document.getElementById('collapseBlock');
 var closeBtn = document.getElementById('closeBtn');
+var flightBtn = document.getElementsByClassName('itemOne');
 
 menu_open[0].addEventListener("click", function () {
     popBlock.style.display = "block";
