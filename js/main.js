78e078e4319166ee77d9c9464b6001750633f42c
Gaurav Sharma <gaurav.sharma@3acestech.com>
2018-06-10 15:12:29 +0530

    website finalizes

--- js/main.js
+++ js/main.js
@@ -16,64 +16,19 @@ closeBtn.addEventListener("click", function () {
 
 })
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-
-
-
-// card layout
-
-$(function() {
-    $('.material-card > .mc-btn-action').click(function () {
-        var card = $(this).parent('.material-card');
-        var icon = $(this).children('i');
-        icon.addClass('fa-spin-fast');
-
-        if (card.hasClass('mc-active')) {
-            card.removeClass('mc-active');
-
-            window.setTimeout(function() {
-                icon
-                    .removeClass('fa-arrow-left')
-                    .removeClass('fa-spin-fast')
-                    .addClass('fa-bars');
-
-            }, 800);
-        } else {
-            card.addClass('mc-active');
-
-            window.setTimeout(function() {
-                icon
-                    .removeClass('fa-bars')
-                    .removeClass('fa-spin-fast')
-                    .addClass('fa-arrow-left');
-
-            }, 800);
-        }
-    });
-});
+$('.cd-testimonials-wrapper').flexslider({
+    //declare the slider items
+    selector: ".cd-testimonials > li",
+    animation: "slide",
+    //do not add navigation for paging control of each slide
+    controlNav: false,
+    slideshow: false,
+    //Allow height of the slider to animate smoothly in horizontal mode
+    smoothHeight: true,
+    start: function(){
+       $('.cd-testimonials').children('li').css({
+          'opacity': 1,
+          'position': 'relative'
+       });
+    }
+ });
